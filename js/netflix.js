e22d1b9a080b4708f810c983646841583c26d0e1
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2013-02-23 19:26:08 +0100

    refactored netflix javascript object

--- js/netflix.js
+++ js/netflix.js
@@ -6,28 +6,43 @@ function NetFlix () {
 NetFlix._BASE_URL		= 'http://odata.netflix.com/';
 
 NetFlix.prototype = {
-	catalog: {
-		titles: {
-			substring: function (term, callback) {
-				url = NetFlix._BASE_URL + 'Catalog/Titles?' +
-					'$filter=substringof(\'' + term + '\',Name)&' +
-					'$callback=callback&' + 
-					'$format=json';
 
-					console.log(url);
+	findMovies: function (titel, callback) {
+		url = NetFlix._BASE_URL + 'Catalog/Titles?' +
+			'$filter=substringof(\'' + titel + '\',Name) and Type eq \'Movie\'&' +
+			'$callback=callback&' + 
+			'$format=json';
 
-				var success = function (data) {
-					callback.call(null, data.d.results);
-				}
+		var success = function (data) {
+			callback.call(null, data.d.results);
+		}
 
-				$.ajax({
-					dataType: 'jsonp',
-					url: url,
-					jsonpCallback: 'callback',
-					success: success
-				});
-			}
-		} 
+		$.ajax({
+			dataType: 'jsonp',
+			url: url,
+			jsonpCallback: 'callback',
+			success: success
+		});
+
+	},
+
+	findSeries: function (titel, callback) {
+		url = NetFlix._BASE_URL + 'Catalog/Titles?' +
+			'$filter=substringof(\'' + titel + '\',Name) and Type eq \'Series\'&' +
+			'$callback=callback&' + 
+			'$format=json';
+
+		var success = function (data) {
+			callback.call(null, data.d.results);
+		}
+
+		$.ajax({
+			dataType: 'jsonp',
+			url: url,
+			jsonpCallback: 'callback',
+			success: success
+		});
+
 	}
 
 
