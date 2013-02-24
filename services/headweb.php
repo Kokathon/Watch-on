393de47735e3db50fbc64294d558b668dff4351d
Oskar Risberg <kokarn@gmail.com>
2013-02-24 13:02:44 +0100

    Fixed headweb names and some formatting

--- services/headweb.php
+++ services/headweb.php
@@ -4,12 +4,16 @@
         private STATIC $apiBase = 'http://www.headweb.com/v4/searchhint/filter(-adult)?apikey=d91b8d77fe2f4c3dbbebbad9ea5dd201&c=0&fields=name&limit=20&offset=0&query=';
         private $movies = array();
 
-        public function searchMovies( $param ){
+        public function searchMovies( $param ) {
             $param = urlencode( $param );
             $data = simplexml_load_file( self::$apiBase . $param );
-            foreach( $data->searchresult->list->content as $result ) :
-                $this->movies[] = array(
-                    'title' => $result->originalname,
+            foreach ( $data->searchresult->list->content as $result ) :
+                if ( isset( $result->originalname ) ) :
+                    $name = (string)$result->originalname; else :
+                    $name = (string)$result->name;
+                endif;
+                $this->movies[ ] = array(
+                    'title' => $name,
                     'type' => 'movie',
                     'service' => 'headweb'
                 );
@@ -17,5 +21,4 @@
 
             return $this->movies;
         }
-
     }
