c24785b6b88b217c41a936346232387e3daf08e0
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2013-02-25 20:29:16 +0100

    made javascript services for all current services

---
+++ js/services/headweb.js
@@ -0,0 +1,57 @@
+
+function headweb () {
+	this._BASE_URL		= 'http://kokarn.com/kokathon/repos/Watch-on/search.php';
+	this._SERVICE_NAME 	= 'headweb';
+}
+
+headweb.prototype = {
+
+	findMovies: function (term, callback) {
+
+		var data = {
+			service: this._SERVICE_NAME,
+			term: term,
+			find: 'movie'
+		}
+
+		$.ajax({
+			dataType: 'jsonp',
+			data: data,
+			url: this._BASE_URL,
+			success: callback
+		});
+	},
+
+	findTv: function (term, callback) {
+		var data = {
+			service: this._SERVICE_NAME,
+			term: term,
+			find: 'tv'
+		}
+
+		$.ajax({
+			dataType: 'jsonp',
+			data: data,
+			url: this._BASE_URL,
+			success: callback
+		});
+	},
+
+	findAll: function (term, callback) {
+		var data = {
+			service: this._SERVICE_NAME,
+			term: term,
+			find: 'all'
+		}
+
+		$.ajax({
+			dataType: 'jsonp',
+			data: data,
+			url: this._BASE_URL,
+			success: callback
+		});
+	} 
+
+
+
+}
