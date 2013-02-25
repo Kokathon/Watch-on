38c859e3a15b5fa750f87e03cb2b8d7c005bedff
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2013-02-25 23:28:59 +0100

    head web extends Service

--- services/headweb.php
+++ services/headweb.php
@@ -1,13 +1,14 @@
 <?php
+    include ( 'service.php' );
 
-    class Headweb {
+    class Headweb extends Service {
         private STATIC $apiBase = 'http://www.headweb.com/v4/searchhint/filter(-adult)?apikey=d91b8d77fe2f4c3dbbebbad9ea5dd201&c=0&fields=name&limit=20&offset=0&query=';
         private static $linkBase = 'http://www.headweb.com/sv/';
         private $movies = array();
 
-        public function searchMovies( $param ) {
-            $param = urlencode( $param );
-            $data = @simplexml_load_file( self::$apiBase . $param );
+        public function findMovies( $term ) {
+            $term = urlencode( $term );
+            $data = @simplexml_load_file( self::$apiBase . $term );
             foreach ( $data->searchresult->list->content as $result ) :
                 if ( isset( $result->originalname ) ) :
                     $name = (string)$result->originalname; else :
@@ -16,11 +17,18 @@
                 $this->movies[ ] = array(
                     'title' => $name,
                     'type' => 'movie',
-                    'service' => 'headweb',
                     'url' => self::$linkBase . (string)$result->attributes()->id
                 );
             endforeach;
 
             return $this->movies;
+        }
+
+        public function findTv($term) {
+            return array();
+        }
+
+        public function findAll($term) {
+            return $this->findMovies($term);
         }
     }
