12d22c824ef994183d7e32e0455c28cc4ef82b00
Oskar Risberg <kokarn@gmail.com>
2013-02-25 20:03:32 +0100

    Added viaplay url's

--- services/viaplay.php
+++ services/viaplay.php
@@ -3,6 +3,7 @@
     class Viaplay {
         private $tvUrl = 'http://viaplay.se/tv/alphabetical';
         private $movieUrl = 'http://viaplay.se/film/samtliga/250/alphabetical';
+        private static $movieBaseUrl = 'http://viaplay.se';
         private $movies = array();
         private $shows = array();
 
@@ -11,9 +12,15 @@
             preg_match_all( '/<ul>(.+?)<\/ul>/s', $data, $matches );
             $filterList = array_slice( $matches[ 0 ], 2, count( $matches ) - 8 );
             foreach ( $filterList as $letterList ) :
-                preg_match_all( '/<a href=".+?">(.+?)<\/a>/s', $letterList, $movies );
-                $this->movies = array_merge( $this->movies, $movies[ 1 ] );
+                preg_match_all( '/<a href="(.+?)">(.+?)<\/a>/s', $letterList, $movies );
+                foreach( $movies[ 2 ] as $key => $movie ) :
+                    $this->movies[] = array(
+                        'title' => $movie,
+                        'url' => self::$movieBaseUrl . $movies[ 1 ][ $key ]
+                    );
+                endforeach;
             endforeach;
+            print_r( $this->movies );
         }
 
         public function findAllTV() {
@@ -21,8 +28,13 @@
             preg_match_all( '/<ul>(.+?)<\/ul>/s', $data, $matches );
             $filterList = array_slice( $matches[ 0 ], 1, count( $matches ) - 8 );
             foreach ( $filterList as $letterList ) :
-                preg_match_all( '/<a href=".+?">(.+?)<\/a>/s', $letterList, $shows );
-                $this->shows = array_merge( $this->shows, $shows[ 1 ] );
+                preg_match_all( '/<a href="(.+?)">(.+?)<\/a>/s', $letterList, $shows );
+                foreach( $shows[ 2 ] as $key => $show ) :
+                    $this->shows[] = array(
+                        'title' => $show,
+                        'url' => self::$movieBaseUrl . $shows[ 1 ][ $key ]
+                    );
+                endforeach;
             endforeach;
         }
 
@@ -34,7 +46,7 @@
             return $this->shows;
         }
 
-        public function search( $param, $type = 'movie' ){
+        public function search( $param, $type = 'movie' ) {
             $m = new MongoClient();
 
             // select a database
@@ -43,23 +55,27 @@
             // select a collection (analogous to a relational database's table)
             $collection = $db->$type;
 
+            // Empty collection
+            $collection->remove();
+
             $condition = new MongoRegex( '/.*' . $param . '.*/i' );
             $findResults = $collection->find( array( 'title' => $condition, 'service' => 'viaplay' ) );
 
             $results = array();
 
-            foreach( $findResults as $result ) :
-                $results[] = array(
+            foreach ( $findResults as $result ) :
+                $results[ ] = array(
                     'title' => $result[ 'title' ],
                     'service' => $result[ 'service' ],
-                    'type' => $type
+                    'type' => $type,
+                    'url' => $result[ 'url' ]
                 );
             endforeach;
 
             return $results;
         }
 
-        public function index(){
+        public function index() {
             $this->findAllMovies();
             // connect
             $m = new MongoClient();
@@ -68,12 +84,18 @@
             $db = $m->watchon;
 
             // select a collection (analogous to a relational database's table)
-            $collection = $db->movie;
+            $collection = $db->viaplaymovie;
+
+            // Empty collection
+            $collection->remove();
+
             // add a record
             foreach ( $this->movies as $movie ) :
                 $document = array(
-                    "title" => $movie,
-                    "service" => "viaplay"
+                    'title' => $movie[ 'title' ],
+                    'service' => 'viaplay',
+                    'type' => 'movie',
+                    'url' => $movie[ 'url' ]
                 );
                 if ( !$collection->findOne( $document ) ) :
                     $collection->insert( $document );
@@ -82,12 +104,18 @@
 
             $this->findAllTV();
 
-            $collection = $db->tv;
+            $collection = $db->viaplaytv;
+
+            // Empty collection
+            $collection->remove();
+
             // add a record
             foreach ( $this->shows as $show ) :
                 $document = array(
-                    "title" => $show,
-                    "service" => "viaplay"
+                    'title' => $show[ 'title' ],
+                    'service' => "viaplay",
+                    'type' => 'tv',
+                    'url' => $show[ 'url' ]
                 );
                 if ( !$collection->findOne( $document ) ) :
                     $collection->insert( $document );
@@ -95,4 +123,8 @@
             endforeach;
         }
     }
+
+    $viaplay = new Viaplay();
+    $viaplay->findAllMovies();
+
 ?>
