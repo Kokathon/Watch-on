e37000596cfb1bb42cc81165a3da7ccd9d490db8
Oskar Risberg <kokarn@gmail.com>
2013-02-25 19:03:01 +0100

    Lovefilm url support

--- services/lovefilm.php
+++ services/lovefilm.php
@@ -9,15 +9,16 @@
         public function searchMovie( $param ){
             $param = urlencode( $param );
             $data = @file_get_contents( $this->searchMovieBase . $param );
-            preg_match_all( '/<h2><a.+?title="(.+?)".+?<\/h2>/s', $data, $matches );
-            foreach( $matches[ 1 ] as $movie ) :
+            preg_match_all( '/<h2><a href="(.+?)"title="(.+?)".+?<\/h2>/s', $data, $matches );
+            foreach( $matches[ 2 ] as $key => $movie ) :
                 if( $movie == 'null' ) :
                     continue;
                 endif;
                 $this->movies[] = array(
                     'title' => utf8_encode( $movie ),
                     'type' => 'movie',
-                    'service' => 'lovefilm'
+                    'service' => 'lovefilm',
+                    'url' => $matches[ 1 ][ $key ]
                 );
             endforeach;
 
@@ -27,15 +28,16 @@
         public function searchTv( $param ) {
             $param = urlencode( $param );
             $data = @file_get_contents( $this->searchTvBase . $param );
-            preg_match_all( '/<h2><a.+?title="(.+?)".+?<\/h2>/s', $data, $matches );
-            foreach( $matches[ 1 ] as $show ) :
+            preg_match_all( '/<h2><a href="(.+?)"title="(.+?)".+?<\/h2>/s', $data, $matches );
+            foreach( $matches[ 2 ] as $key => $show ) :
                 if( $show == 'null' ) :
                     continue;
                 endif;
                 $this->shows[] = array(
                     'title' => utf8_encode( $show ),
                     'type' => 'tv',
-                    'service' => 'lovefilm'
+                    'service' => 'lovefilm',
+                    'url' => $matches[ 1 ][ $key ]
                 );
             endforeach;
 
