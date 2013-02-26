cabbc76899c2f9c439af8cd716e0cc42bace5224
Oskar Risberg <kokarn@gmail.com>
2013-02-26 19:11:36 +0100

    Ensure search return data
    Fix error when either tv or movie search doesn't return an array

--- services/service.php
+++ services/service.php
@@ -1,15 +1,25 @@
 <?php
 
-abstract class Service {
+    abstract class Service {
 
-	abstract public function findMovies($term);
-	abstract public function findTv($term);
+        abstract public function findMovies( $term );
 
-	public function findAll($term) {
-		return array_merge($this->findMovies($term), $this->findTv($term));
-	}
-		
-}
+        abstract public function findTv( $term );
 
+        public function findAll( $term ) {
+            $movies = $this->findMovies( $term );
+            $tv = $this->findTv( $term );
+
+            if ( is_array( $movies ) && is_array( $tv ) ) :
+                return array_merge( $movies, $tv );
+            elseif ( is_array( $movies ) ) :
+                return $movies;
+            elseif ( is_array( $tv ) ) :
+                return $tv;
+            else :
+                return false;
+            endif;
+        }
+    }
 
 ?>
