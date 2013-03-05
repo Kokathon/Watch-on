ac5fcd53ab51827b9633c30a9b471d2144ac05ed
Oskar Risberg <kokarn@gmail.com>
2013-03-05 19:51:22 +0100

    Cleanup of HBO service. Closes #8

--- services/hbo.service.php
+++ services/hbo.service.php
@@ -1,14 +1,14 @@
 <?php
     require_once ( 'service.php' );
 
-    class Hbo extends Service {
+    class Hbo extends Service implements Indexable {
         private $tvUrl = 'http://hbonordic.com/rest-services-hook/series';
         private $movieUrl = 'http://hbonordic.com/rest-services-hook/movies?startIndex=0&count=5000';
-        private static $movieBaseUrl = 'http://hbonordic.com';
+        private static $baseUrl = 'http://hbonordic.com';
         private $movies = array();
         private $shows = array();
 
-        public function findAllMovies(){
+        public function indexMovies(){
             $data = @file_get_contents( $this->movieUrl );
             $moviesResult = json_decode( $data );
 
@@ -17,53 +17,6 @@
                 $movies[] = $movie;
             endforeach;
 
-            $this->movies = $movies;
-        }
-
-        public function findAllTV(){
-            $data = @file_get_contents( $this->tvUrl );
-            $showsResult = json_decode( $data );
-
-            $shows = array();
-            foreach( $showsResult as $show ) :
-                $shows[] = $show;
-            endforeach;
-
-            $this->shows = $shows;
-        }
-
-        public function getMovies() {
-            return $this->movies;
-        }
-
-        public function getShows() {
-            return $this->shows;
-        }
-
-        public function search( $term, $type = 'movie' ){
-            $m = new MongoClient();
-
-            // select a database
-            $db = $m->watchon;
-
-            // select a collection (analogous to a relational database's table)
-            $collectionName = 'hbo' . $type;
-            $collection = $db->$collectionName;
-
-            $condition = new MongoRegex( '/.*' . $term . '.*/i' );
-            $resultsCursor = $collection->find( array( 'title' => $condition, 'service' => 'hbo' ) );
-
-            $results = array();
-
-            foreach( $resultsCursor as $result ) :
-                $results[] = $result;
-            endforeach;
-
-            return $results;
-        }
-
-        public function index(){
-            $this->findAllMovies();
             // connect
             $m = new MongoClient();
 
@@ -76,31 +29,12 @@
             $collection->remove();
 
             // add a record
-            foreach ( $this->movies as $movie ) :
+            foreach ( $movies as $movie ) :
                 $document = array(
                     'title' => $movie->title,
                     'type' => 'movie',
                     'service' => 'hbo',
-                    'url' => self::$movieBaseUrl . $movie->url
-                );
-                if ( !$collection->findOne( $document ) ) :
-                    $collection->insert( $document );
-                endif;
-            endforeach;
-
-            $this->findAllTV();
-
-            $collection = $db->hbotv;
-
-            $collection->remove();
-
-            // add a record
-            foreach ( $this->shows as $show ) :
-                $document = array(
-                    'title' => $show->title,
-                    'type' => 'tv',
-                    'service' => 'hbo',
-                    'url' => self::$movieBaseUrl . $show->url
+                    'url' => self::$baseUrl . $movie->url
                 );
                 if ( !$collection->findOne( $document ) ) :
                     $collection->insert( $document );
@@ -108,6 +42,44 @@
             endforeach;
         }
 
+        public function indexTv(){
+            $data = @file_get_contents( $this->tvUrl );
+            $showsResult = json_decode( $data );
+
+            $shows = array();
+            foreach( $showsResult as $show ) :
+                $shows[] = $show;
+            endforeach;
+
+            // connect
+            $m = new MongoClient();
+
+            // select a database
+            $db = $m->watchon;
+
+            // select a collection (analogous to a relational database's table)
+            $collection = $db->hbotv;
+
+            $collection->remove();
+
+            // add a record
+            foreach ( $shows as $show ) :
+                $document = array(
+                    'title' => $show->title,
+                    'type' => 'tv',
+                    'service' => 'hbo',
+                    'url' => self::$baseUrl . $show->url
+                );
+                if ( !$collection->findOne( $document ) ) :
+                    $collection->insert( $document );
+                endif;
+            endforeach;
+        }
+
+        public function createIndex(){
+            $this->indexMovies();
+            $this->indexTv();
+        }
 
         public function findMovies($term) {
             $m = new MongoClient();
